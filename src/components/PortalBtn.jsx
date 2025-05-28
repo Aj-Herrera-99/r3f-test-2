7d40eeedfeb7f205b640dfb0f37a09faf772a9e0
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-05-28 03:39:27 +0200

    test

--- src/components/PortalBtn.jsx
+++ src/components/PortalBtn.jsx
@@ -2,6 +2,6 @@ import { Html } from "@react-three/drei";
 
 export default function PortalBtn({ content, onClick }) {
     return (
-        <Html className="-translate-x-1/2" position={[0, window.innerWidth > 640 ? -3.75 : -3.6, -.5]}><button onClick={onClick} className="whitespace-nowrap pb-1 px-2 border-b scale-95 hover:scale-100 transition-transform max-sm:text-xs  cursor-pointer">{content}</button></Html>
+        <Html className="-translate-x-1/2" position={[0, window.innerWidth > 640 ? -3.75 : -3.6, -.5]}><button onClick={onClick} className="whitespace-nowrap pb-1 px-2 hover:border-b active:border-b scale-95 active:scale-100 hover:scale-100 transition-transform max-sm:text-xs  cursor-pointer">{content}</button></Html>
     )
 }
