a101327105ce3904b93ce679154ff53671d1e0a4
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-05-30 03:20:33 +0200

    testq

--- src/utils/takeScreenshot.js
+++ src/utils/takeScreenshot.js
@@ -4,11 +4,12 @@ export const takeScreenshot = (gl, scene, camera, setState) => {
     setTimeout(() => {
         // screenshot
         gl.render(scene, camera);
-        html2canvas(document.body).then((canvas) => {
-            const dataURL = canvas.toDataURL("image/png");
-            setState(dataURL);
-            // download, save, etc.
-        });
-        // const dataUrl = gl.domElement.toDataURL("image/png");
+        // html2canvas(document.body).then((canvas) => {
+        //     const dataURL = canvas.toDataURL("image/png");
+        //     setState(dataURL);
+        //     // download, save, etc.
+        // });
+        const dataUrl = gl.domElement.toDataURL("image/png");
+        setState(dataUrl);
     }, 600);
 };
